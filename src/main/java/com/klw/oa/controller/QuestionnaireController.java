7b817a57e668995c42a4b3b3fded71b6d5c7ab50
wx-5078 <wx-kuanglw@dtdream.com>
2017-04-24 20:00:03 +0800

    问卷查询controller修改

--- src/main/java/com/klw/oa/controller/QuestionnaireController.java
+++ src/main/java/com/klw/oa/controller/QuestionnaireController.java
@@ -1,7 +1,15 @@
 package com.klw.oa.controller;
 
+import com.klw.oa.entity.Question;
+import com.klw.oa.entity.Questionnaire;
+import com.klw.oa.service.QuestionnaireService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by admins on 2017/4/22.
@@ -9,4 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/questionnaire")
 public class QuestionnaireController {
+
+    @Autowired
+    QuestionnaireService questionnaireService;
+
+    @ResponseBody
+    @RequestMapping("/getAll")
+    public List<Questionnaire> getQuestionnaires(Questionnaire questionnaire){
+        List<Questionnaire> questionnaires = questionnaireService.getAllByPage(questionnaire,0,3);
+
+        return questionnaires;
+    }
+
+    @ResponseBody
+    @RequestMapping("/getById")
+    public Questionnaire getComplexQuestionnaire(@RequestParam(value = "questionnaireId") Integer questionnaireId){
+        Questionnaire questionnaire = questionnaireService.getComplexById(questionnaireId);
+
+        return questionnaire;
+    }
 }
